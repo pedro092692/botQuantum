04726dc2bc07bed29ccd1b3a56ef56ff28f622b1
pedro <pedro092692@gmail.com>
2025-01-21 23:26:31 +0100

    update main

--- main.py
+++ main.py
@@ -4,5 +4,3 @@ from data_df import DataProcess
 btc_info = GetData(exchange='binance', symbol='btc', timeframe='5m', candles=450)
 data = DataProcess(symbol_data=btc_info.price_data(), info=btc_info)
 data.save_data_to_csv()
-
-print('this is the bot')
