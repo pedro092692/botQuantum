e7174d03ec7b28c17c6f516f7627e04d8e16a38e
pedro <pedro092692@gmail.com>
2025-01-21 22:11:07 +0100

    add get_data class

---
+++ get_data.py
@@ -0,0 +1,27 @@
+import ccxt
+
+
+class GetData:
+    def __init__(self, exchange, symbol: str, timeframe, candles):
+        self.exchange = exchange
+        self.symbol = symbol.upper() + '/USDT'
+        self.timeframe = timeframe
+        self.candles = candles
+
+    def exchange_info(self):
+        exchange_name = self.exchange
+        exchange_class = getattr(ccxt, exchange_name)
+        return exchange_class()
+
+    def price_data(self):
+        ohlcv = self.exchange_info().fetch_ohlcv(self.symbol, self.timeframe, limit=self.candles)
+        return ohlcv
+
+
+
+
+
+
+
+
+
