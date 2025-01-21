52e3c82ac7b33c471d9a72a0bc461f2a0eb6998c
pedro <pedro092692@gmail.com>
2025-01-21 23:17:35 +0100

    add data_df file

---
+++ data_df.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+class DataProcess:
+    def __init__(self, symbol_data, info):
+        self.symbol_data = symbol_data
+        self.info = info
+
+    def convert_data(self) -> pd.DataFrame:
+        dataframe = pd.DataFrame(self.symbol_data)
+        dataframe.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
+        # convert timestamp in datetime object
+        dataframe.date = pd.to_datetime(dataframe.date, unit='ms')
+        dataframe.date = dataframe.date.dt.strftime('%d-%m-%Y %H:%M')
+        return dataframe
+
+    def save_data_to_csv(self):
+        df = self.convert_data()
+        new_order = ['open', 'high', 'low', 'close', 'date', 'volume']
+        df = df[new_order]
+        time = df.date.iloc[-1]
+        df.to_csv(f'markets_data/{self.info.symbol.replace('/', '-')}-{self.info.timeframe}-{time}.csv',
+                  sep=',', header=True, index=False)
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from get_data import GetData
+from data_df import DataProcess
 
+btc_info = GetData(exchange='binance', symbol='btc', timeframe='5m', candles=450)
+data = DataProcess(symbol_data=btc_info.price_data(), info=btc_info)
+data.save_data_to_csv()
