41a96b0f0db5dd5b6af478c5f8d73e5b9271452b
pedro <pedro092692@gmail.com>
2025-02-12 23:31:27 +0100

    create strategy.py and add more indicators in indicator class

--- Indicator.py
+++ Indicator.py
@@ -10,3 +10,34 @@ class Indicator:
     def candle_indicators(self, pattern: str):
         self.df_info.ta.cdl_pattern(name=pattern, append=True)
         return self.df_info
+
+    def bollinger_bands(self, bb_len, n_std, add_to_df=False):
+        bb = ta.bbands(
+            self.df_info['close'],
+            length=bb_len,
+            std=n_std
+        )
+        if add_to_df:
+            # add actual indicator to current symbol data
+            self.df_info['lbb'] = bb.iloc[:, 0]
+            self.df_info['mbb'] = bb.iloc[:, 1]
+            self.df_info['ubb'] = bb.iloc[:, 2]
+
+            print('Bollinger Bands Added to symbol data frame')
+        else:
+            return bb
+
+    def rsi(self, rsi_len: int, add_to_df=False):
+        rsi = ta.rsi(
+            self.df_info['close'],
+            length=rsi_len
+        )
+
+        if add_to_df:
+            # add actual indicator to current symbol data
+            self.df_info['rsi'] = rsi
+            print('RSI Added to symbol data frame')
+        else:
+            return rsi
+
+
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from get_data import GetData
 from data_df import DataProcess
 from Indicator import Indicator
+
 # Get exchange
-exchange = GetData(exchange='binance', symbol='ltc', timeframe='15m', candles=300)
+exchange = GetData(exchange='binance', symbol='eth', timeframe='15m', candles=100)
+
 # Get ohlcv data from exchange
 symbol_data = DataProcess(symbol_data=exchange.get_ohlcv(), info=exchange)
+
 # add ohlcv df to indicators class
 indicator = Indicator(df_inf=symbol_data.to_df())
+
 # add doji patter indicator to de ohlcv data
-doji_pattern = indicator.candle_indicators(pattern='doji')
+# doji_pattern = indicator.candle_indicators(pattern='doji')
+
+# calculate bb_bands
+bb_bands = indicator.bollinger_bands(bb_len=20, n_std=2.0, add_to_df=True)
+rsi = indicator.rsi(rsi_len=14, add_to_df=True)
+
+
 # sava df in csv format
-symbol_data.save_data_to_csv(custom_df=doji_pattern)
+symbol_data.save_data_to_csv(custom_df=indicator.df_info)
