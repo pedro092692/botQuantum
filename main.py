23f182cfbeae3bb39cb90c426389c8b6db1d237a
pedro <pedro092692@gmail.com>
2025-02-13 10:36:13 +0100

    add check long signal with in strategy class

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from get_data import GetData
 from data_df import DataProcess
-from Indicator import Indicator
+from indicator import Indicator
+from strategy import Strategy
+
 
 # Get exchange
 exchange = GetData(exchange='binance', symbol='eth', timeframe='15m', candles=100)
@@ -16,8 +18,18 @@ indicator = Indicator(df_inf=symbol_data.to_df())
 
 # calculate bb_bands
 bb_bands = indicator.bollinger_bands(bb_len=20, n_std=2.0, add_to_df=True)
+# calculate rsi
 rsi = indicator.rsi(rsi_len=14, add_to_df=True)
 
+# save dataframe with symbol indicators
+symbol_data_with_indicators = indicator.df_info
+
+# set up strategy
+strategy = Strategy(symbol_data=symbol_data_with_indicators)
+
+# start strategy
+strategy.check_long_signal(over_bought_rsi=60, over_sold_rsi=40)
 
 # sava df in csv format
-symbol_data.save_data_to_csv(custom_df=indicator.df_info)
+symbol_data.save_data_to_csv(custom_df=symbol_data_with_indicators)
+
--- strategy.py
+++ strategy.py
@@ -1,3 +1,19 @@
 import pandas as pd
 import pandas_ta as ta
 
+
+class Strategy:
+    def __init__(self, symbol_data):
+        self.df = symbol_data
+
+    def check_long_signal(self, over_bought_rsi, over_sold_rsi):
+        # add long signal column to df
+        self.df['long_signal'] = ''
+
+        for index in range(len(self.df)):
+            # check for long signal in data
+            if (over_bought_rsi > self.df['rsi'].iloc[index] > over_sold_rsi) and \
+                    (self.df['low'].iloc[index - 1] < self.df['lbb'].iloc[index - 1]) and \
+                    (self.df['low'].iloc[index] > self.df['lbb'].iloc[index]):
+
+                self.df.loc[index, 'long_signal'] = 'Buy'
